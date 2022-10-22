5f49c6f4901de0d3f0cc8c1a2a26b64e7102b917
Kyle VanTassell <octicon@gmail.com>
2022-10-22 11:14:46 -0500

    added useEffect, useDispatch to UserPage.jsx

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import LocationItem from '../LocationItem/LocationItem';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const locationList = useSelector(store => store.location);
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_LOCATIONS' });
+  }, []);
+
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
-      <div>locations go here</div>
+      <div>
+        {locationList.map(location => {
+          return (
+            <LocationItem key={location.id} location={location} />
+          );
+        })}
+      </div>
       {/* use effect / axios get for info in datadase */}
       {/* button to add new location  / POST */}
 
