90ec6680b2f8a9f04315ad0b3ecb3380f777cc4e
Kyle VanTassell <octicon@gmail.com>
2022-10-22 17:04:21 -0500

    locationsSaga

---
+++ src/redux/sagas/locations.saga.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+function* fetchAllLocations() {
+    try {
+        const locations = yield axios.get('/api/locations');
+        console.log('get all:', locations.data);
+        yield put({ type: 'SET_LOCATIONS', payload: locations.data});
+    } catch {
+        console.log('get locations error');
+    }
+}
+
+function* locationsSaga() {
+    yield takeLatest('GET_LOCATIONS', fetchAllLocations);
+  }
+
+export default locationsSaga;
