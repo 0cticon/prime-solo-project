47fecec59d8bb108a849f0c4778137ca6766cd8e
Kyle VanTassell <octicon@gmail.com>
2022-10-28 17:09:44 -0500

    details component sizing and centering css

--- src/components/LocationDetails/LocationDetails.jsx
+++ src/components/LocationDetails/LocationDetails.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 import { useEffect } from 'react';
+import './LocationDetails.css';
 
 function LocationDetails() {
     const location = useSelector(store => store.locationDetails);
@@ -12,8 +13,8 @@ function LocationDetails() {
     }, [locationId]);
 
     return (
-        <div>
-            <h3>{location.activity} {location.location}</h3>
+        <div className="location">
+            <h2>{location.activity}</h2> <h4>located at</h4> <h3>{location.location}</h3>
             <a href={`https://www.google.com/maps/dir/?api=1&destination=${location.location}`}>Open Map</a>
         </div>
     )
